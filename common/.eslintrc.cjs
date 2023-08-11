a1f7b5f8e965990f6c5b918c0311bd858fadf1be
horiy0125 <k.horiy0125@gmail.com>
2023-08-11 23:23:09 +0900

    chore: modify .eslintrc.cjs

--- common/.eslintrc.cjs
+++ common/.eslintrc.cjs
@@ -12,8 +12,6 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
     ecmaVersion: 2021,
     sourceType: "module",
     ecmaFeatures: {
