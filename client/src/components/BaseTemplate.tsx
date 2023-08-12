6d5285c363b50dd5e806ff84f23f845dd3710aef
horiy0125 <k.horiy0125@gmail.com>
2023-08-13 01:01:38 +0900

    chore: delete Nav component

--- client/src/components/BaseTemplate.tsx
+++ client/src/components/BaseTemplate.tsx
@@ -1,16 +1,9 @@
 import { PropsWithChildren } from 'react';
-import { Nav } from './Nav';
 
 type Props = PropsWithChildren;
 
 export const BaseTemplate: React.FC<Props> = props => {
   const { children } = props;
 
-  return (
-    <>
-      <Nav />
-
-      <main className="container">{children}</main>
-    </>
-  );
+  return <main className="container">{children}</main>;
 };
--- client/src/components/index/IndexTemplate.tsx
+++ client/src/components/index/IndexTemplate.tsx
@@ -3,10 +3,7 @@ import { BaseTemplate } from '../BaseTemplate';
 export const IndexTemplate: React.FC = () => {
   return (
     <BaseTemplate>
-      <div className="headings">
-        <h1>horiylabs</h1>
-        <p>ようこそ</p>
-      </div>
+      <h1>horiylabs</h1>
     </BaseTemplate>
   );
 };
